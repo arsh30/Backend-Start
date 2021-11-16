f2353c75baabcf99f732200cb445303b87af54cb
Arsh <as3096734@gmail.com>
2021-11-15 20:51:55 -0800

    upto1st class finish

--- foodApp/server.js
+++ foodApp/server.js
@@ -5,12 +5,35 @@ app.listen('5000', function () {
     console.log("server is listening on 5000");
 })
 
+app.use(express.json()); 
 let user = {};
 
-app.get('/', function (req, res) {
+app.get('/', function (req, res) {  //[server send data to client]
     res.send('Home Page');
 })
 app.get('/user', function (req, res) {
     res.json(user);
 });
 
+//post request [client -> server ko data bhjta hai or and the data comes in the request]
+app.post('/user', function (req, res) {
+    user = req.body;
+    console.log(req.body);
+    res.send("data has been added successfully")
+})
+
+//patch req  [it will update by adding something]
+app.patch('/user', function (req, res) {  
+    let obj = req.body;   
+    for (let key in obj) {  
+        user[key] = obj;   
+                            
+    }
+    res.json(user)
+})
+
+//delete req
+app.delete('/user', function (req, res) {
+    user = {};
+    res.json(user);
+})
