37275b762feaee498e0e997daddc9d4056dab1a0
Arsh <as3096734@gmail.com>
2021-11-16 17:19:40 -0800

    3rd class upto redirect

--- foodApp/server.js
+++ foodApp/server.js
@@ -1,47 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-app.listen('5000', function () {
-    console.log("server is listening on 5000");
-})
+app.listen("5000", function () {
+  console.log("server is listening on 5000");
+});
 
 app.use(express.json());
-app.use(express.static('public')); 
-const userRouter = express.Router();  //used to create the router when multiple routes(get,post,patch,delete) has created.Now we specify the route that our this userRouter have and userRouter.route().post()
+app.use(express.static("public"));
+const userRouter = express.Router(); //used to create the router when multiple routes(get,post,patch,delete) has created.Now we specify the route that our this userRouter have and userRouter.route().post()
 const authRouter = express.Router();
 
-app.use('/user', userRouter);
-app.use('/auth', authRouter);
+app.use("/user", userRouter);
+app.use("/auth", authRouter);
 
 //mounting in express
 userRouter
-    .route('/')
-    .get(getUser) 
-    .post(createUser)
-    .patch(updateUser)
-    .delete(deleteUser);
+  .route("/")
+  .get(getUser)
+  .post(createUser)
+  .patch(updateUser)
+  .delete(deleteUser);
 
-userRouter
-    .route('/:id')
-    .get(getUserbyId)
+userRouter.route("/:id").get(getUserbyId);
 
-authRouter
-    .route('/signup')
-    .post(signupUser);
+authRouter.route("/signup").post(signupUser);
 
 let user = [];
 
 // A
-app.get('/', function (req, res) {  //[server send data to client]
-    res.send('Home Page');
-})
+app.get("/", function (req, res) {
+  //[server send data to client]
+  res.send("Home Page");
+});
 
 //(CRUD OPERATION starts)
 
 //read
 // app.get('/user', getUser);
 function getUser(req, res) {
-    res.json(user); //B -> A nd B are same but we write in a different way
+  res.json(user); //B -> A nd B are same but we write in a different way
 }
 
 //post request [client -> server ko data bhjta hai or and the data comes in the request]
@@ -49,49 +46,55 @@ function getUser(req, res) {
 
 // app.post('/user', createUser);
 function createUser(req, res) {
-    user = req.body;
-    console.log(req.body);
-    res.send("data has been added successfully")
+  user = req.body;
+  console.log(req.body);
+  res.send("data has been added successfully");
 }
 
 //patch req  [it will update by adding something]
 
 // app.patch('/user', updateUser);
-function updateUser (req, res) {  
-    let obj = req.body;   
-    for (let key in obj) {  
-        user[key] = obj;   
-                            
-    }
-    res.json(user)
-};
+function updateUser(req, res) {
+  let obj = req.body;
+  for (let key in obj) {
+    user[key] = obj;
+  }
+  res.json(user);
+}
 //delete req
 // app.delete('/user', deleteUser);
 function deleteUser(req, res) {
-    user = {};
-    res.json(user);
+  user = {};
+  res.json(user);
 }
 
 //parameter route
 // app.get('/user/:id', getUserbyId);
-function getUserbyId(req, res) {  
-    console.log(req.params);  
-    res.send(req.params);
+function getUserbyId(req, res) {
+  console.log(req.params);
+  res.send(req.params);
 }
-
 
 function signupUser(req, res) {
-    // let userDetails = req.body;
-    // let name = userDetails.name;
-    // let email = userDetails.email;
-    // let password = userDetails.password; 
-    let { email, name, password } = req.body;
-    user.push({ email, name, password });
-    console.log('user', req.body);
-    res.json({
-        message: "user Signedup",
-        user: req.body 
-    })
+  // let userDetails = req.body;
+  // let name = userDetails.name;
+  // let email = userDetails.email;
+  // let password = userDetails.password;
+  let { email, name, password } = req.body;
+  user.push({ email, name, password });
+  console.log("user", req.body);
+  res.json({
+    message: "user Signedup",
+    user: req.body,
+  });
 }
 
+//Redirect
+app.get("/user-all", function (req, res) {
+  res.redirect("/user");
+});
 
+//404 Page
+app.use(function (req, res) {
+  res.sendFile("./public/404.html", { root: __dirname });
+});
