2af94431b4febdcee45a9a682761795b25f08aeb
Arsh <as3096734@gmail.com>
2021-11-17 04:26:46 -0800

    class3 upto forgetPassword_practice

--- foodApp/server.js
+++ foodApp/server.js
@@ -6,10 +6,22 @@ app.listen("5000", function () {
 });
 
 app.use(express.json());
+app.use(function (req, res,next) {  //this middleware aate hi run hojega or niche kuch chlega hi nhi ab isme 3rd parameter hot hai next
+   //do some work
+    console.log("i am a middleware function");
+    next();  //yeh next krne se next function execute hojega
+});
+
 app.use(express.static("public"));
-const userRouter = express.Router(); //used to create the router when multiple routes(get,post,patch,delete) has created.Now we specify the route that our this userRouter have and userRouter.route().post()
+const userRouter = express.Router(); 
 const authRouter = express.Router();
 
+// app.use(function (req, res,next) { 
+//     //do some work
+//      console.log("i am a middleware function 2nd time");
+//      next(); 
+// });
+ 
 app.use("/user", userRouter);
 app.use("/auth", authRouter);
 
@@ -21,9 +33,18 @@ userRouter
   .patch(updateUser)
   .delete(deleteUser);
 
-userRouter.route("/:id").get(getUserbyId);
+userRouter
+    .route("/:id")
+    .get(getUserbyId);
 
-authRouter.route("/signup").post(signupUser);
+authRouter
+    .route("/signup")
+    .post(signupUser);
+
+authRouter
+    .route('/forgetPassword')
+    .get(getForgetPassword)
+    .post(postForgetPassword,validateEmail);
 
 let user = [];
 
@@ -38,7 +59,8 @@ app.get("/", function (req, res) {
 //read
 // app.get('/user', getUser);
 function getUser(req, res) {
-  res.json(user); //B -> A nd B are same but we write in a different way
+  console.log("getUser called");
+    res.json(user); //B -> A nd B are same but we write in a different way
 }
 
 //post request [client -> server ko data bhjta hai or and the data comes in the request]
@@ -85,8 +107,34 @@ function signupUser(req, res) {
   console.log("user", req.body);
   res.json({
     message: "user Signedup",
-    user: req.body,
+    user : req.body,  
   });
+}
+
+function getForgetPassword(req, res) {  //fetch only file
+    res.sendFile('/public/forgetPassword.html', { root: __dirname });
+}
+function postForgetPassword(req, res , next) {
+    let data = req.body;
+
+    console.log('data', data);
+    next(); //upr pass krege
+    //  res.json({
+    //     message: "data recieved",
+    //     data: data.email  
+    // })
+}
+
+function validateEmail(req, res) { 
+    console.log('validate function email');
+    console.log(req.body);
+
+    //to check email is correct or not means @ hai ya nahi or ya (.com) ho
+ 
+    res.json({
+        message: "data recieved",
+        data: req.body
+    })
 }
 
 //Redirect
